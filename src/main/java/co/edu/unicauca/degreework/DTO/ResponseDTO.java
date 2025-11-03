599d813e5bf021895beb5750b1ec2ee4f25ad5ef
juanserealpe <juansevarona@hotmail.com>
2025-11-03 11:43:24 -0500

    Auto stash before checking out "HEAD"

--- src/main/java/co/edu/unicauca/degreework/DTO/ResponseDTO.java
+++ src/main/java/co/edu/unicauca/degreework/DTO/ResponseDTO.java
@@ -7,15 +7,15 @@ public class ResponseDTO {
     private Long id;
     private String title;
     private String description;
-    private String status;
-    private String process;
+    private Status status;
+    private Process process;
 
     public ResponseDTO(Long id, String title, String description, Status status, Process process) {
         this.id = id;
         this.title = title;
         this.description = description;
-        this.status = adaptStatus(status);
-        this.process = prettyProcess(process);
+        this.process = process;
+        this.status = status;
     }
     public ResponseDTO() {}
 
@@ -24,24 +24,7 @@ public class ResponseDTO {
     public String getDescription(){return description;}
     public Long getId() { return id; }
     public String getTitle() { return title; }
-    public String getStatus() { return status; }
-    public String getProcess() { return process; }
-
-    private String adaptStatus(Status status) {
-        return switch (status) {
-            case CREATED -> " ";
-            case FORMAT_A, DRAFT -> "EN PROCESO";
-            case FORMAT_A_ACCEPTED, APROVED -> "APROBADO";
-            case INACTIVE -> "DESAPROBADO";
-        };
-    }
-
-    private String prettyProcess(Process process) {
-        return switch (process){
-            case FORMATO_A -> "FORMATO A";
-            case ANTEPROYECTO -> "ANTEPROYECTO";
-            case INACTIVO -> " ";
-        };
-    }
+    public Status getStatus() { return status; }
+    public Process getProcess() { return process; }
 }
 
