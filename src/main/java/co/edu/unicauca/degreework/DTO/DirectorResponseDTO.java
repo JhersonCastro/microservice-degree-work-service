d49a4ed14de359b271cf1c9b2c8c69c635e0aae8
juanserealpe <juansevarona@hotmail.com>
2025-11-02 23:21:29 -0500

    add empty constructor into DirectorResponseDTO
    it's a json library rule

--- src/main/java/co/edu/unicauca/degreework/DTO/DirectorResponseDTO.java
+++ src/main/java/co/edu/unicauca/degreework/DTO/DirectorResponseDTO.java
@@ -1,6 +1,5 @@
 package co.edu.unicauca.degreework.DTO;
 
-import co.edu.unicauca.degreework.Enum.Process;
 import co.edu.unicauca.degreework.Enum.Status;
 
 public class DirectorResponseDTO {
@@ -9,17 +8,19 @@ public class DirectorResponseDTO {
     private String description;
     private Status status;
 
-    public DirectorResponseDTO(Long id, String title, String description, Status status) {
+    public DirectorResponseDTO(Long id, String title,String description, Status status) {
         this.id = id;
         this.title = title;
         this.description = description;
         this.status = status;
     }
+    public DirectorResponseDTO() {}
+
 
     // Getters
+    public String getDescription(){return description;}
     public Long getId() { return id; }
     public String getTitle() { return title; }
-    public String getDescription() { return description; }
     public Status getStatus() { return status; }
 }
 
